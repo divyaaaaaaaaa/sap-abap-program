65e9817b728d17ddf30f66f5653994b136fdb7aa
divya panduranga naik <61453134+divyaaaaaaaaa@users.noreply.github.com>
2025-12-21 12:26:58 +0530

    Remove default Python file

--- swapping.py
+++ swapping.py
@@ -1,24 +1,3 @@
-a,b=3,5
-print(f"before swapping value a & b is {a} & {b}")
-a,b=b,a
-print(f"after swapping a & b is {a} & {b}")
 
-#swappint without third variable
-a1=4
-b1=6
-print(f"before swapping value a & b is {a1} & {b1}")
-a1=a1+b1
-b1=a1-b1
-a1=a1-b1
-print(f"after swapping a & b is {a1} & {b1}")
 
-#swapping with using third variable
-a11=7
-b11=9
-print(f"before swapping the value of a and b will be {a11},{b11}")
-temp=0
-temp = a11
-a11 = b11
-b11 = temp
-print(f"after swapping the value of a and b will be {a11},{b11}")
 
